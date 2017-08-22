8f605a2254b76f585a75c86967547737326f0ba6
neex-io <code@neex.io>
2017-08-21 22:27:05 -0700

    Access query parameters

--- src/main/java/io/neex/hellospring/controllers/HelloController.java
+++ src/main/java/io/neex/hellospring/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package io.neex.hellospring.controllers;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,13 +8,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import javax.swing.*;
 
 @Controller
+// @RequestMapping("hello") puts hello in path before every other route in this controller
 public class HelloController {
 
     @RequestMapping(value = "")
     @ResponseBody
-    public String index() {
-        return "Hello Spring";
+    public String index(HttpServletRequest request) { // access query
+
+        String name = request.getParameter("name"); // fetch query value for name
+
+        if (name == null) { // safeguard when no param given
+            name = "World";
+        }
+        return "Hello " + name;
     }
+
+    // POST REQUEST
+    public String HelloForm() {
+
+    }
+
+
 
     @RequestMapping(value = "goodbye")
     @ResponseBody
